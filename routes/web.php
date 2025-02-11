ec24f2baa297970aee409e5cad1d46d3ab29cb0e
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-02-11 12:34:38 -0300

    FEAT:(Controller/Rote) Adding metodes controller and incrementing rotes.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'home'])->name('home');
+Route::post('/generate-exercices', [MainController::class, 'generateExercises'])->name('generateExercises');
+Route::get('/print-exercices', [MainController::class, 'printExercises'])->name('printExercises');
+Route::get('/export-exercices', [MainController::class, 'exportExercises'])->name('exportExercises');
