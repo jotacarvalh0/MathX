544c96dd5bd05eaad3975d98223cd8a543b09d5e
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-02-19 12:22:03 -0300

    FEAT:(View) Creating the operations view.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,7 +12,7 @@ class MainController extends Controller
         return view('home');
     }
 
-    public function generateExercises(Request $request)
+    public function generateExercises(Request $request): View
     {
         // form validation
         $request->validate([
@@ -91,7 +91,7 @@ class MainController extends Controller
         }
 
 
-        dd($exercises);
+        return view('operations', ['exercises' => $exercises]);
     }
 
     public function printExercises()
